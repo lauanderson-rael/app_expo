260525962dcd4e4d0bce8812fa6df817060b5b09
Lauanderson <lauanderson@okngroup.com.br>
2025-09-25 19:48:05 -0300

    :white_check_mark: aula 06 - JSX

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello World</Text>
+    <View style={css.container}>
+      <Text style={css.title}>Olá mundo</Text>
     </View>
   );
 }
+
+const css = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#01041b",
+    alignItems: "center",
+    
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+});
